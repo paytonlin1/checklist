102f776ba1d7b915fde0e1d2a9713deb4ddc92b8
paytonlin1 <plinteq14@icloud.com>
2024-09-10 12:08:56 -0400

    Add function to return key

--- source.js
+++ source.js
@@ -1,3 +1,9 @@
 function addListItem() {
     const newListItem = document.createElement("listItem");
 }
+document.getElementsById("keyInput").addEventListener("keydown", function(e) {
+    var keyCode = e.keyCode || e.which;
+    if(keyCode == 13) {
+        addListItem();
+    }
+}, false);
